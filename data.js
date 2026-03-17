4a319923472d2326271dd3890a4a9df04e01f1ec
videra12-max <videra12@gmail.com>
2026-03-17 21:58:09 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,3 @@
 const teams = [
-  { name:"Persija", logo:"https://upload.wikimedia.org/wikipedia/id/7/70/Persija_Jakarta_logo.svg" },
-  { name:"Persib", logo:"assets/logo/persib.png" },
-  { name:"Persebaya", logo:"https://upload.wikimedia.org/wikipedia/id/2/2e/Persebaya_logo.svg" },
-  { name:"Arema", logo:"https://upload.wikimedia.org/wikipedia/id/8/86/Arema_FC_2017_logo.svg" }
+    { name:"Persib", logo:"assets/logo/persib.png" }
 ];
