d86379816c044bd50f5bb4e26de6332e658a9bea
videra12-max <videra12@gmail.com>
2026-03-17 22:03:19 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,3 +1,9 @@
 const teams = [
-    { name:"Persib", logo:"assets/logo/persib.png" }
+  { id:1, name:"Persija", logo:"assets/logo/persija.png" },
+  { id:2, name:"Persib", logo:"assets/logo/persib.png" },
+  { id:3, name:"Persebaya", logo:"assets/logo/persebaya.png" },
+  { id:4, name:"Arema", logo:"assets/logo/arema.png" }
 ];
+
+// load data match dari localStorage
+let matches = JSON.parse(localStorage.getItem("matches")) || [];
